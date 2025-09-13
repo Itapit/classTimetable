4efd3fba9b45c718f970429886387fbe6d8d86c6
itapit <Itamardavid90@gmail.com>
2025-09-13 15:36:20 +0300

    minor changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
-import Nora from '@primeuix/themes/nora';
+import Aura from '@primeuix/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
@@ -17,11 +17,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Nora,
+        preset: Aura,
         options: {
-          prefix: 'p',
           darkModeSelector: 'system',
-          cssLayer: false,
         },
       },
     }),
