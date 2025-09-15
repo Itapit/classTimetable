bfa9d8e49066e301e619b240d5eeef0dfcd2aacb
devshaki <guy@shoko.co.il>
2025-09-15 12:12:20 +0300

    minor changes

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DAILY_QUOTES } from './quotes';
 
 @Component({
   selector: 'app-title',
@@ -10,17 +11,16 @@ import { CommonModule } from '@angular/common';
 export class TitleComponent {
   text = 'הציטוט היומי';
 
-  quotes: string[] = [
-    'אני בשירותים (ירדן דרורי)',
-    'לא טוב',
-    'מוביט זה פרוקסי של איראן',
-  ];
-
   quote: string;
 
   constructor() {
     // Pick a quote based on the day of year for daily rotation
+<<<<<<< Updated upstream
     const day = Math.floor(Date.now() / 86400000);
     this.quote = this.quotes[day % this.quotes.length];
+=======
+    const day = Math.floor((Date.now() / 86400000));
+    this.quote = DAILY_QUOTES[day % DAILY_QUOTES.length];
+>>>>>>> Stashed changes
   }
 }
